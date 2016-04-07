bef6f9e95bff291ffae54b713d8b89d92f412e3f
Herberth Amaral <herberth.amaral@sisqualis.com.br>
2016-04-07 01:12:00 -0300

    Robot can move in any direction now

--- robot.py
+++ robot.py
@@ -3,6 +3,12 @@ def robot(position, movements):
         if m == 'M':
             if position[2] == 'N':
                 position[1] += 1
+            if position[2] == 'S':
+                position[1] -= 1
+            if position[2] == 'E':
+                position[0] += 1
+            if position[2] == 'W':
+                position[0] -= 1
         if m == 'L':
             if position[2] == 'N':
                 position[2] = 'W'
--- test_robot.py
+++ test_robot.py
@@ -5,7 +5,7 @@ def test_rover_without_moves_should_do_nothing():
     final_position = robot(position=position, movements=[])
     assert position == final_position
 
-def test_rover_facing_north_should_increment_y_by_one_when_moves():
+def test_rover_facing_north_should_increment_y_by_one_when_it_moves():
     position=[1,2,'N']
     final_position = robot(position=position, movements=['M'])
     assert final_position == [1, 3, 'N']
@@ -14,3 +14,18 @@ def test_rover_facing_north_should_face_west_when_rotate_to_left():
     position=[1,2,'N']
     final_position = robot(position=position, movements=['L'])
     assert final_position == [1, 2, 'W']
+
+def test_rover_facing_south_should_decrement_y_by_one_when_it_moves():
+    position=[1,2,'S']
+    final_position = robot(position=position, movements=['M'])
+    assert final_position == [1, 1, 'S']
+
+def test_rover_facing_east_should_increment_x_by_one_when_it_moves():
+    position=[1,2,'E']
+    final_position = robot(position=position, movements=['M'])
+    assert final_position == [2, 2, 'E']
+
+def test_rover_facing_west_should_decrement_x_by_one_when_it_moves():
+    position=[1,2,'W']
+    final_position = robot(position=position, movements=['M'])
+    assert final_position == [0, 2, 'W']
