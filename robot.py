61773a9f583a9b682f06606e9fe0a043e4029d87
Herberth Amaral <herberth.amaral@sisqualis.com.br>
2016-04-07 07:47:51 -0300

    Refact to use a decision table when moving

--- robot.py
+++ robot.py
@@ -2,17 +2,14 @@ TURNS = {
     'L':{'N':'W', 'E':'N', 'S':'E', 'W':'S'},
     'R':{'N':'E', 'E':'S', 'S':'W', 'W':'N'}
 }
+MOVEMENTS = {
+    'N': {'y':1}, 'S':{'y': -1}, 'E': {'x':1}, 'W':{'x':-1}
+}
 def robot(position, movements, debug=False):
     for m in movements:
         if m == 'M':
-            if position[2] == 'N':
-                position[1] += 1
-            elif position[2] == 'S':
-                position[1] -= 1
-            elif position[2] == 'E':
-                position[0] += 1
-            elif position[2] == 'W':
-                position[0] -= 1
+            position[0] += MOVEMENTS[position[2]].get('x', 0)
+            position[1] += MOVEMENTS[position[2]].get('y', 0)
         elif m == 'L':
             position[2] = TURNS[m][position[2]]
         elif m == 'R':
