62be1b25065fb863ab2fdf040ddd0853ac35d7e6
Herberth Amaral <herberth.amaral@sisqualis.com.br>
2016-04-07 00:55:26 -0300

    Robot can now move north

--- robot.py
+++ robot.py
@@ -1,2 +1,6 @@
 def robot(position, movements):
+    for m in movements:
+        if m == 'M':
+            if position[2] == 'N':
+                position[1] += 1
     return position
--- test_robot.py
+++ test_robot.py
@@ -1,6 +1,11 @@
 from robot import robot
 
 def test_rover_without_moves_should_do_nothing():
-    position=(1,2,'N')
+    position=[1,2,'N']
     final_position = robot(position=position, movements=[])
     assert position == final_position
+
+def test_rover_facing_north_should_increment_y_by_one_when_moves():
+    position=[1,2,'N']
+    final_position = robot(position=position, movements=['M'])
+    assert final_position == [1, 3, 'N']
