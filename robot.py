89230efaa267dff831189f43c04d5974448acefb
Herberth Amaral <herberth.amaral@sisqualis.com.br>
2016-04-07 01:19:27 -0300

    Robot can now turn to right flawlessly

--- robot.py
+++ robot.py
@@ -3,13 +3,22 @@ def robot(position, movements):
         if m == 'M':
             if position[2] == 'N':
                 position[1] += 1
-            if position[2] == 'S':
+            elif position[2] == 'S':
                 position[1] -= 1
-            if position[2] == 'E':
+            elif position[2] == 'E':
                 position[0] += 1
-            if position[2] == 'W':
+            elif position[2] == 'W':
                 position[0] -= 1
-        if m == 'L':
+        elif m == 'L':
             if position[2] == 'N':
                 position[2] = 'W'
+        elif m == 'R':
+            if position[2] == 'N':
+                position[2] = 'E'
+            elif position[2] == 'E':
+                position[2] = 'S'
+            elif position[2] == 'S':
+                position[2] = 'W'
+            elif position[2] == 'W':
+                position[2] = 'N'
     return position
--- test_robot.py
+++ test_robot.py
@@ -29,3 +29,23 @@ def test_rover_facing_west_should_decrement_x_by_one_when_it_moves():
     position=[1,2,'W']
     final_position = robot(position=position, movements=['M'])
     assert final_position == [0, 2, 'W']
+
+def test_rover_facing_north_should_face_east_when_rotate_to_right():
+    position=[1,2,'N']
+    final_position = robot(position=position, movements=['R'])
+    assert final_position == [1, 2, 'E']
+
+def test_rover_facing_east_should_face_south_when_rotate_to_right():
+    position=[1,2,'E']
+    final_position = robot(position=position, movements=['R'])
+    assert final_position == [1, 2, 'S']
+
+def test_rover_facing_south_should_face_west_when_rotate_to_right():
+    position=[1,2,'S']
+    final_position = robot(position=position, movements=['R'])
+    assert final_position == [1, 2, 'W']
+
+def test_rover_facing_west_should_face_north_when_rotate_to_right():
+    position=[1,2,'W']
+    final_position = robot(position=position, movements=['R'])
+    assert final_position == [1, 2, 'N']
